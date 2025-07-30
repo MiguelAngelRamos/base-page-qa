eeb0f6e2a6cb8479e8b30329f73719488528cae9
Miguel Ramos <return.infox@gmail.com>
2025-07-29 20:26:14 -0400

    localizadores

--- src/test/java/cl/kibernumacademy/basepage/pages/LoginPage.java
+++ src/test/java/cl/kibernumacademy/basepage/pages/LoginPage.java
@@ -1,5 +1,10 @@
 package cl.kibernumacademy.basepage.pages;
 
-public class LoginPage {
-  
+public class LoginPage extends BasePage {
+  // Localizadores
+  private String emailField = "//input[@id='email']";
+  private String passwordField = "//input[@id='password']";
+  public void navigateToLogin() {
+    navigateTo("https://testing-qa.netlify.app/pages/login");
+  }
 }
